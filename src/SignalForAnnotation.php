437b5e03ebd9335ff8c1a1022108c7bd8b2ce641
Peter Maselkowski <pmaselkowski@gmail.com>
2015-01-15 15:03:31 +0100

    + Headers + hedron - Removed gii

--- src/SignalForAnnotation.php
+++ src/SignalForAnnotation.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * This software package is licensed under GNU LESSER GENERAL PUBLIC LICENSE license.
+ *
+ * @package maslosoft/signals
+ * @licence GNU LESSER GENERAL PUBLIC LICENSE
+ *
+ * @copyright Copyright (c) Peter Maselkowski <pmaselkowski@gmail.com>
+ *
+ */
+
 namespace Maslosoft\Signals;
 
 /**
