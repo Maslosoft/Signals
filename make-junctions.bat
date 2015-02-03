00474618c7b82f3c9be4b78db741812c012f2d72
Peter Maselkowski <pmaselkowski@gmail.com>
2015-02-03 15:35:04 +0100

    * Normalize class names #5 ^ dep

--- make-junctions.bat
+++ make-junctions.bat
@@ -2,3 +2,4 @@ mklink /j unit vendor\maslosoft\signalstest\tests\unit
 mklink /j models vendor\maslosoft\signalstest\models
 mklink /j signals vendor\maslosoft\signalstest\signals
 mklink /j extensions vendor\maslosoft\signalstest\extensions
+mklink /j runtime vendor\maslosoft\signalstest\runtime
