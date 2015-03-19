2dccb59ca0d56b31ef65a3d8ba25115e39526683
Peter Maselkowski <pmaselkowski@gmail.com>
2015-03-19 18:41:30 +0100

    +~ CLI application draft

---
+++ src/Commands/PreviewCommand.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Maslosoft\Signals\Commands;
+
+use Exception;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * PreviewCommand
+ *
+ * @author Piotr Maselkowski <pmaselkowski at gmail.com>
+ */
+/**
+ * PreviewCommand
+ *
+ * @author Piotr Maselkowski <pmaselkowski at gmail.com>
+ */
+class PreviewCommand extends Command
+{
+
+	protected function configure()
+	{
+		$this->setName("preview");
+		$this->setDescription("Show list of signals and slots");
+		$this->setDefinition([
+		]);
+
+		$help = <<<EOT
+The <info>preview</info> command will display list of signals and slots.
+				No files will be modified at this stage.
+EOT;
+		$this->setHelp($help);
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		throw new Exception('TODO');
+	}
+
+	/**
+	 * @SlotFor(Maslosoft\Sitcom\Command)
+	 * @param Maslosoft\Signals\Command $signal
+	 */
+	public function reactOn(\Maslosoft\Sitcom\Command $signal)
+	{
+		$signal->add($this, 'hedron');
+	}
+}
+
