1f68e3d66cbda10e6f5591d61b7ae81653bbcdae
Peter Maselkowski <pmaselkowski@gmail.com>
2015-06-08 11:36:35 +0200

    + make-junctions.sh

---
+++ make-junctions.sh
@@ -0,0 +1,5 @@
+ln -s vendor/maslosoft/signalstest/tests/unit unit
+ln -s vendor/maslosoft/signalstest/models models
+ln -s vendor/maslosoft/signalstest/signals test-signals
+ln -s vendor/maslosoft/signalstest/extensions extensions
+ln -s vendor/maslosoft/signalstest/runtime runtime
