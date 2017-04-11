a03e425256ea13441c318a087bf7f6a15ae586a4
Peter Maselkowski <pmaselkowski@gmail.com>
2017-04-11 12:18:16 +0200

    * Better processing errors detection

---
+++ src/Exceptions/ClassNotFoundException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Maslosoft\Signals\Exceptions;
+
+use Exception;
+
+/**
+ * ClassNotFoundException
+ *
+ * @author Piotr Maselkowski <pmaselkowski at gmail.com>
+ */
+class ClassNotFoundException extends Exception
+{
+
+}
