0a9b1c653c96d99eebf5905df95c3339eaaac00b
Peter Maselkowski <office@maslosoft.com>
2018-01-11 14:01:13 +0100

    ! Don't show so many errors - Removed accidentally committed files

--- src/Builder/Addendum.php
+++ src/Builder/Addendum.php
@@ -13,6 +13,7 @@
 namespace Maslosoft\Signals\Builder;
 
 use Exception;
+use Maslosoft\Addendum\Exceptions\NoClassInFileException;
 use Maslosoft\Addendum\Exceptions\ParseException;
 use Maslosoft\Addendum\Interfaces\AnnotatedInterface;
 use Maslosoft\Addendum\Utilities\AnnotationUtility;
@@ -29,6 +30,7 @@ use Maslosoft\Signals\Meta\SignalsMeta;
 use Maslosoft\Signals\Signal;
 use ParseError;
 use Psr\Log\LoggerInterface;
+use ReflectionClass;
 use ReflectionException;
 use UnexpectedValueException;
 
@@ -100,6 +102,10 @@ class Addendum implements ExtractorInterface
 	{
 		// These are loaded in some other way...
 		if ($className === 'PHP_Invoker')
+		{
+			return false;
+		}
+		if ($className === '\PHP_Invoker')
 		{
 			return false;
 		}
@@ -187,6 +193,11 @@ class Addendum implements ExtractorInterface
 		{
 			$annotated = AnnotationUtility::rawAnnotate($file);
 		}
+		catch(NoClassInFileException $e)
+		{
+			$this->log($e, $file);
+			return;
+		}
 		catch (ClassNotFoundException $e)
 		{
 			$this->log($e, $file);
@@ -218,7 +229,7 @@ class Addendum implements ExtractorInterface
 
 		try
 		{
-			// NOTE: This autloader must be registered on ReflectionClass
+			// NOTE: This autoloader must be registered on ReflectionClass
 			// creation ONLY! That's why register/unregister.
 			// This will detect not found depending classes
 			// (base classes,interfaces,traits etc.)
@@ -226,6 +237,7 @@ class Addendum implements ExtractorInterface
 			spl_autoload_register($autoload);
 			eval('$info = new ReflectionClass($fqn);');
 			spl_autoload_unregister($autoload);
+			echo "$file\n";
 		}
 		catch (ParseError $e)
 		{
@@ -242,6 +254,8 @@ class Addendum implements ExtractorInterface
 			$this->err($e, $file);
 			return;
 		}
+		// $info is created in `eval`
+		/* @var $info ReflectionClass */
 		$isAnnotated = $info->implementsInterface(AnnotatedInterface::class);
 		$hasSignals = $this->hasSignals($contents);
 		$isAbstract = $info->isAbstract() || $info->isInterface();
@@ -361,11 +375,46 @@ class Addendum implements ExtractorInterface
 		$this->signal->getLogger()->warning($msg);
 	}
 
+	private function debug($e, $file)
+	{
+		/* @var $e ParseError|Exception */
+		$msg = sprintf('Warning: %s while scanning file `%s`', $e->getMessage(), $file);
+		$msg = $msg . PHP_EOL;
+		$this->signal->getLogger()->debug($msg);
+	}
+
 	private function err($e, $file)
 	{
+		// Uncomment for debugging
+//		if(strstr($e->getMessage(), 'PHP_Invoker'))
+//		{
+//			$traces = [];
+//			foreach(debug_backtrace() as $trace)
+//			{
+//				$file = @$trace['file'];
+//				$line = @$trace['line'];
+//				$traces[] = "$file: $line";
+//			}
+//			print_r($traces);
+//			exit;
+//		}
 		/* @var $e ParseError|Exception */
 		$msg = sprintf('Error: %s while scanning file `%s`', $e->getMessage(), $file);
 		$msg = $msg . PHP_EOL;
+
+		// Don't output errors on Maslosoft test models
+		if(preg_match('~Maslosoft\\\\\w+Test~', $e->getMessage()))
+		{
+			$this->signal->getLogger()->debug($msg);
+			return;
+		}
+
+		// Skip PHP_Invoker class, as it is possibly deprecated anyway
+		if(strstr($e->getMessage(), 'PHP_Invoker'))
+		{
+			$this->signal->getLogger()->debug($msg);
+			return;
+		}
 		$this->signal->getLogger()->error($msg);
 	}
 
